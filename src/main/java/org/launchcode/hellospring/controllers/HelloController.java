efdc0c8581d138ed61624bfc631db650ef43edba
bonniewhy <bonnieinpixels@gmail.com>
2019-01-17 13:16:03 -0600

    Adding TODOs for bonus missions in an attempt to understand everything better

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,6 +1,7 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,53 +30,72 @@ public class HelloController {
 
         String html = "<form method='post'>" +
                 "<input type='text' name='name' />" +
-                "<select id='lang'>" +
+                "<select name='lang'>" +
                 "<option value='eng'>English</option>" +
-                "<option value='spa'>Spanish</option>" +
                 "<option value='fre'>French</option>" +
-                "<option value='jap'>Japanese</option>" +
+                "<option value='spa'>Spanish</option>" +
                 "<option value='rus'>Russian</option>" +
-                "</select>" +
+                "<option value='jap'>Japanese</option>" +
                 "<input type='submit' value='Greet Me!' />" +
                 "</form>";
 
         return html;
+
     }
 
     @RequestMapping(value = "hello", method = RequestMethod.POST)
     @ResponseBody
-    // public String helloPost(HttpServletRequest request) {
+    public String helloPost(HttpServletRequest request) {
 
-    // }
-
-    public static String createMessage(HttpServletRequest request) {
         String name = request.getParameter("name");
         String lang = request.getParameter("lang");
 
-        if (lang == "eng") {
+        // [ ] Bonus Mission 1: Add a bit of HTML to the response string to make it look nicer.
+
+        // [ ] Bonus Mission 2: Restructure the code so that the controller class doesn't know anything about the specific languages available. In other words, it asks the model for available languages to present to the user.
+
+        // [ ] Bonus Mission 3: Add some additional output that displays the number of times the user has been greeted. Hint: Use a static property to keep track of the count.
+
+        // [ ] Bonus Mission 4: The bonus mission above doesn't discriminate between requests made by you or somebody else. In other words, it counts total greetings rather than greetings to a specific user. Fix this by using cookies. (See original instructions for how to do this...)
+
+        if (lang.equals("eng")) {
+
             return "Hello, " + name + "!";
-        }
-        else if (lang == "spa") {
-            return "Hola, " + name + "!";
-        }
-        else if (lang == "fre") {
+
+        } else if (lang.equals("fre")) {
+
             return "Bonjour, " + name + "!";
-        }
-        else if (lang == "jap") {
-            return "Konnichiwa, " + name + "!";
-        }
-        else if (lang == "rus") {
+
+        } else if (lang.equals("spa")) {
+
+            return "Hola, " + name + "!";
+
+        } else if (lang.equals("rus")) {
+
             return "Privet, " + name + "!";
+
+        } else if (lang.equals("jap")) {
+
+            return "Konnichiwa, " + name + "!";
+
+        } else {
+
+            return "I'm sorry. I don't know that language. D:";
         }
-        else {
-            return "Sorry, I don't know that language. D:";
-        }
+
+    }
+
+    @RequestMapping(value = "hello/{name}")
+    @ResponseBody
+    public String helloUrlSegment(@PathVariable String name) {
+
+        return "Hello, " + name + "!";
+
     }
 
     @RequestMapping(value = "goodbye")
-    @ResponseBody
     public String goodbye() {
-        return "Goodbye...";
+        return "redirect:/";
     }
 
 }
